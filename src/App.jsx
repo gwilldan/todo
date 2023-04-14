087467b48da9122b9090f7260e61edfc924366f0
gwilldan <sayhi2godswill@gmail.com>
2023-04-14 16:13:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      APP.JSX
+    </div>
+  )
+}
+
+export default App
